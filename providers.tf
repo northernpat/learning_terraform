8639a01cd0301d436bdf341f102e364f0dd4da07
northernpat <northernpat@yahoo.ca>
2022-11-07 00:45:20 -0500

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 
 provider "aws" {
  region  = "ca-central-1"
-  access_key = "AWS_ACCESS_KEY_ID"
-   secret_key = "AWS_SECRET_ACCESS_KEY"
+  access_key = "$AWS_ACCESS_KEY_ID"
+   secret_key = "$AWS_SECRET_ACCESS_KEY"
   
 }
